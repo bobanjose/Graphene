3129536aba7759c9707d6c38d18e3376b7e3394f
Steve Little <Steven.Little@relayhealth.com>
2014-02-07 15:50:29 -0700

    Added Service API

---
+++ src/Graphene.API/Controllers/GraphenePersistController.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Web.Http;
+using Graphene.Data;
+using Graphene.Publishing;
+
+namespace Graphene.API.Controllers
+{
+    public class GraphenePersistController : ApiController
+    {
+        private static readonly IPersist MongoPersist =
+            new PersistToMongo(ConfigurationManager.ConnectionStrings["MongoConnectionString"].ConnectionString);
+
+        // POST api/values
+        public void Post([FromBody] TrackerData trackerData)
+        {
+            MongoPersist.Persist(trackerData);
+        }
+    }
+}
