9298655825e380bca53dd810a20d1eed1efb2a43
Steve Little <Steven.Little@relayhealth.com>
2014-02-18 14:24:52 -0700

    Added Service Persister

---
+++ src/Graphene/Publishing/PersistToService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using Graphene.Data;
+
+namespace Graphene.Publishing
+{
+    class PersistToService : IPersist
+    {
+        private string _serviceUrl;
+
+        public PersistToService(string serviceUrl)
+        {
+            _serviceUrl = serviceUrl;
+        }
+
+        public void Persist(TrackerData trackerData)
+        {
+            // setup the client
+            // -   with a custome handler to approve all server certificates
+            var handler = new WebRequestHandler
+            {
+                ServerCertificateValidationCallback =
+                    (sender, certificate, chain, errors) => true
+            };
+            try
+            {
+                using (var client = new HttpClient(handler))
+                {
+                    // post it
+                    var httpResponseMessage = client.PostAsJsonAsync(_serviceUrl, trackerData).Result;
+                    if (!httpResponseMessage.IsSuccessStatusCode)
+                    {
+                        Configurator.Configuration.Logger.Warn(string.Format("Data not persisted with status {0}", httpResponseMessage.StatusCode));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Configurator.Configuration.Logger.Error(ex.Message, ex);
+            }
+        }
+    }
+}
