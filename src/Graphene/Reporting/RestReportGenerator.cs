c2c1120b571c21bb725b8926483df4bcdd1a85c9
Matthew Snow <matthew.snow@relayhealth.com>
2014-03-05 10:34:24 -0800

    Adding attribute for identifying measurable counters via reflection for reporting. Renamed and seperated Interfaces and smaller classes

---
+++ src/Graphene/Reporting/RestReportGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using Graphene.Exceptions;
+using Graphene.Tracking;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Graphene.Reporting
+{
+    using MongoDB.Driver.Builders;
+
+    public interface IReportGenerator
+    {
+        IEnumerable<IAggregationResult> GeneratorReport(IReportSpecification specification);
+
+    }
+
+    public class RestReportGenerator : IReportGenerator
+    {
+        private readonly Uri _serviceUri;
+
+
+        public RestReportGenerator(Uri serviceUri)
+        {
+            _serviceUri = serviceUri;
+        }
+
+
+        public IEnumerable<IAggregationResult> GeneratorReport(IReportSpecification specification)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var task = httpClient.PostAsJsonAsync(_serviceUri.ToString(), specification)
+                                         .ContinueWith(x => x.Result.Content.ReadAsAsync<IEnumerable<IAggregationResult>>().Result);
+
+                task.ContinueWith(x =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        if (task.Exception != null)
+                            throw new ReportGenerationException(task.Exception);
+
+                    }
+                    IEnumerable<IAggregationResult> results = task.Result;
+                    return results;
+                });
+
+                task.Wait();
+
+            }
+            return null;
+        }
+    }
+
+
+}
+
