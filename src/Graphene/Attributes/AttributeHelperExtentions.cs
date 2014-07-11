132851f8398e4e7ce1bdef129b57fd71d95d15ce
boban.jose@relayhealth.com <boban.jose@relayhealth.com>
2014-07-11 16:59:38 -0700

    Added TrackerMetaDataAttribute to decorate a Graphene Attribute with Name, Description etc

---
+++ src/Graphene/Attributes/AttributeHelperExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphene.Attributes
+{
+    public static class AttributeHelperExtentions
+    {
+        public static TValue GetAttributeValue<TAttribute, TValue>(this Type type, Func<TAttribute, TValue> valueOf) where TAttribute : Attribute
+        {
+            var attribute = type.GetCustomAttributes(typeof(TAttribute), true).OfType<TAttribute>().FirstOrDefault();
+
+            return attribute != null ? valueOf(attribute) : default(TValue);
+        }
+    }
+}
---
+++ src/Graphene/Attributes/TrackerMetaDataAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphene.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class TrackerMetaDataAttribute : Attribute
+    {
+        
+        public TrackerMetaDataAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ToolTip { get; set; }
+
+        public string Legend { get; set; }
+    }
+}
